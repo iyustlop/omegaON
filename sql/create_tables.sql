5e0213576c589bccc929f60c0abdb163585efb67
Ignacio Yuste Lopez <iyustlop@gmail.com>
2025-01-11 18:06:43 +0100

    refine sql file

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS workers;
+
 -- Crear la tabla 'workers'
-CREATE TABLE workers (
+CREATE TABLE workers(
     id VARCHAR(50) PRIMARY KEY,      -- Identificador único como string
     name VARCHAR(100) NOT NULL,      -- Nombre del trabajador
     office VARCHAR(50),              -- Oficina del trabajador
@@ -11,9 +13,9 @@ CREATE TABLE workers (
 
 -- Insertar dos registros de ejemplo
 INSERT INTO workers (id, name, office, location, room, on_boarding, last_change)
-VALUES
-    ('T1000','Alice Johnson', 'London', 'ShortForm', 'Alpha', '2023-05-01', '2025-01-01'),
-    ('T1001','Bob Smith', 'Madrid', 'LongForm', 'Kappa', '2022-11-15', '2025-01-01');
+VALUES ('T1000','Alice Johnson', 'London', 'ShortForm', 'Alpha', '2023-05-01', '2025-01-01');
+INSERT INTO workers (id, name, office, location, room, on_boarding, last_change)
+VALUES ('T1001','Bob Smith', 'Madrid', 'LongForm', 'Kappa', '2022-11-15', '2025-01-01');
 
 -- Seleccionar datos para confirmar inserciones (opcional)
 SELECT * FROM workers;
